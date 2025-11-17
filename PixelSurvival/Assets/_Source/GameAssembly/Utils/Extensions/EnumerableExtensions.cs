8e30e715ac2c2770a3f221671a9cf54786b4fb36
Алексей <ix.fenix@mail.ru>
2025-11-17 17:48:37 +0300

    feat: layers & enumerable utils

---
+++ PixelSurvival/Assets/_Source/GameAssembly/Utils/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace GameAssembly.Utils.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static T GetRandomElement<T>(this IEnumerable<T> source)
+        {
+            GetRandom(source, out var item, out _, out _);
+            return item;
+        }
+        
+        public static int GetRandomIndex<T>(this IEnumerable<T> source)
+        {
+            GetRandom(source, out _, out var index, out _);
+            return index;
+        }
+        
+        public static T GetRandomElement<T>(this IEnumerable<T> source, out T[] asArray)
+        {
+            GetRandom(source, out var item, out _, out var array);
+            asArray = array;
+            return item;
+        }
+        
+        public static int GetRandomIndex<T>(this IEnumerable<T> source, out T[] asArray)
+        {
+            GetRandom(source, out _, out var index, out var array);
+            asArray = array;
+            return index;
+        }
+
+        private static void GetRandom<T>(IEnumerable<T> source, out T item, out int index, out T[] asArray)
+        {
+            asArray = source.ToArray();
+            item = asArray[Random.Range(0, asArray.Length)];
+            index = Random.Range(0, asArray.Length);
+        }
+    }
+}
