e3ab8699c97287e18c0a5232ca783626f877406a
Алексей <ix.fenix@mail.ru>
2025-11-18 20:15:05 +0300

    feat: pixel-perfect camera & player camera

---
+++ PixelSurvival/Assets/_Source/GameAssembly/PlayerSystem/PlayerCamera.cs
@@ -0,0 +1,19 @@
+﻿using Mirror;
+using Unity.Cinemachine;
+
+namespace GameAssembly.PlayerSystem
+{
+    public class PlayerCamera : NetworkBehaviour
+    {
+        private CinemachineCamera _playerCamera;
+
+        private void Start()
+        {
+            if(!isLocalPlayer)
+                return;
+            
+            _playerCamera = FindFirstObjectByType<CinemachineCamera>();
+            _playerCamera.Target = new CameraTarget { TrackingTarget = transform };
+        }
+    }
+}
