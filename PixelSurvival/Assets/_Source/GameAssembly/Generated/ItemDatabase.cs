cdc07bb04a121ab156cc94725e22f8ad54e20825
Алексей <ix.fenix@mail.ru>
2026-04-08 19:18:02 +0300

    feat: new block & objects & recipe configs

--- PixelSurvival/Assets/_Source/GameAssembly/Generated/ItemDatabase.cs
+++ PixelSurvival/Assets/_Source/GameAssembly/Generated/ItemDatabase.cs
@@ -9,34 +9,36 @@ namespace GameAssembly.Generated
     public static class ItemDatabase
     {
         public static readonly ItemDefinitionSO Apple;
-        public static readonly ItemDefinitionSO Axe;
-        public static readonly ItemDefinitionSO Chest;
         public static readonly ItemDefinitionSO Dirt;
-        public static readonly ItemDefinitionSO Pickaxe;
+        public static readonly ItemDefinitionSO WoodenLogBlock;
+        public static readonly ItemDefinitionSO WoodenPlanksBlock;
+        public static readonly ItemDefinitionSO Chest;
+        public static readonly ItemDefinitionSO Furnace;
+        public static readonly ItemDefinitionSO Workbecnh;
         public static readonly ItemDefinitionSO Pie;
         public static readonly ItemDefinitionSO RedMushroom;
         public static readonly ItemDefinitionSO Stick;
         public static readonly ItemDefinitionSO Stone;
         public static readonly ItemDefinitionSO Stump;
-        public static readonly ItemDefinitionSO WoodenLogBlock;
-        public static readonly ItemDefinitionSO WoodenPlanksBlock;
-        public static readonly ItemDefinitionSO Workbecnh;
+        public static readonly ItemDefinitionSO Axe;
+        public static readonly ItemDefinitionSO Pickaxe;
 
         static ItemDatabase()
         {
             Apple = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/Apple");
-            Axe = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/Axe");
-            Chest = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/Chest");
             Dirt = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/Dirt");
-            Pickaxe = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/Pickaxe");
+            WoodenLogBlock = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/WoodenLogBlock");
+            WoodenPlanksBlock = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/WoodenPlanksBlock");
+            Chest = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/Chest");
+            Furnace = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/Furnace");
+            Workbecnh = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/Workbecnh");
             Pie = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/Pie");
             RedMushroom = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/RedMushroom");
             Stick = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/Stick");
             Stone = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/Stone");
             Stump = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/Stump");
-            WoodenLogBlock = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/WoodenLogBlock");
-            WoodenPlanksBlock = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/WoodenPlanksBlock");
-            Workbecnh = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/Workbecnh");
+            Axe = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/Axe");
+            Pickaxe = Resources.Load<ItemDefinitionSO>(AssetsPaths.ITEM_CONFIGS_PATH + "/Pickaxe");
         }
     }
 }
