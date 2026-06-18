17d9f431325249a0ce64e764845e6da1b1a32932
Алексей <ix.fenix@mail.ru>
2026-06-18 09:32:21 +0300

    feat: objects shadows

---
+++ PixelSurvival/Assets/_Source/GameAssembly/Utils/SinMover.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace GameAssembly.Utils
+{
+    public class SinMover : MonoBehaviour
+    {
+        [SerializeField] private Transform moveObject;
+        
+        [Header("Floating")]
+        [SerializeField] private float floatAmplitude = 5f;
+        [SerializeField] private float floatSpeed = 2f;
+
+        private Vector3 _startPosition;
+
+        private void Start()
+        {
+            _startPosition =  moveObject.localPosition;
+        }
+
+        private void Update()
+        {
+            var offsetY = Mathf.Sin(Time.time * floatSpeed) * floatAmplitude;
+
+            moveObject.localPosition = _startPosition + Vector3.up * offsetY;
+        }
+    }
+}
--- PixelSurvival/Assets/_Source/GameAssembly/Utils/Ui/ArrowButton.cs
+++ PixelSurvival/Assets/_Source/GameAssembly/Utils/Ui/ArrowButton.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace GameAssembly.Utils.Ui
@@ -6,9 +7,53 @@ namespace GameAssembly.Utils.Ui
     public class ArrowButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         [SerializeField] private GameObject arrow;
+        [SerializeField] private Transform buttonTransform;
 
-        public void OnPointerEnter(PointerEventData eventData) => arrow.gameObject.SetActive(true);
+        [Header("Floating")]
+        [SerializeField] private float floatAmplitude = 5f;
+        [SerializeField] private float floatSpeed = 2f;
+        [SerializeField] private float upSize = 2f;
+        [SerializeField] private float sizeUpTime = 0.25f;
 
-        public void OnPointerExit(PointerEventData eventData) => arrow.gameObject.SetActive(false);
+        private bool _isSelected;
+        private Vector3 _startPosition;
+        private Vector3 _startScale;
+        private Tween _tween;
+
+        private void Awake()
+        {
+            _startPosition = buttonTransform.localPosition;
+            _startScale = buttonTransform.localScale;
+        }
+
+        private void Update()
+        {
+            if (!_isSelected)
+            {
+                buttonTransform.localPosition = _startPosition;
+                return;
+            }
+
+            var offsetY = Mathf.Sin(Time.time * floatSpeed) * floatAmplitude;
+
+            buttonTransform.localPosition = _startPosition + Vector3.up * offsetY;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            arrow.SetActive(true);
+            _isSelected = true;
+            _tween = buttonTransform.DOScale(Vector3.one * upSize, sizeUpTime);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            arrow.SetActive(false);
+            _isSelected = false;
+            buttonTransform.localPosition = _startPosition;
+            _tween?.Kill();
+            _tween = null;
+            buttonTransform.localScale = _startScale;
+        }
     }
 }
